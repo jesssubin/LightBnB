d68a6ef7bffa5163f4c85118be6beeaf8982f728
jesssubin <jess.s.moon@gmail.com>
2020-10-15 17:26:16 +0000

    Update functions to use lightbnb database with SQL queries

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -16,27 +16,44 @@ const pool = new Pool({
  * @param {String} email The email of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
+
+const getUserWithEmail = function (email) {
+  return pool.query(`
+  SELECT * FROM users
+  WHERE email = $1;
+  `, [email])
+    .then(res => {
+      console.log(res); 
+      if (res.rows.length) {
+        return res.rows[0];
+      } else {
+        return null;
+      }
+    });
 }
 exports.getUserWithEmail = getUserWithEmail;
+
+
+
 
 /**
  * Get a single user from the database given their id.
  * @param {string} id The id of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+const getUserWithId = function (id) {
+  return pool.query(`
+  SELECT * FROM users
+  WHERE id = $1;
+  `, [id])
+    .then(res => {
+      console.log(res);
+      if (res.rows.length) {
+        return res.rows[0];
+      } else {
+        return null;a
+      }
+    });
 }
 exports.getUserWithId = getUserWithId;
 
@@ -46,11 +63,21 @@ exports.getUserWithId = getUserWithId;
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
  */
-const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+const addUser = function (user) {
+  const queryString = `
+  INSERT INTO users (name, email, password) 
+  VALUES ($1, $2, $3)
+  RETURNING *
+  `; 
+  const values = [user.name, user.email, user.password];
+  return pool.query(queryString, values)
+    .then(res => {
+      if (res.rows.length) {
+        return res.rows[0];
+      } else {
+        return null;
+      }
+    });
 }
 exports.addUser = addUser;
 
--- LightBnB_WebApp-master/server/userRoutes.js
+++ LightBnB_WebApp-master/server/userRoutes.js
@@ -24,8 +24,10 @@ module.exports = function(router, database) {
    * @param {String} password encrypted
    */
   const login =  function(email, password) {
+  
     return database.getUserWithEmail(email)
     .then(user => {
+      // console.log(user); 
       if (bcrypt.compareSync(password, user.password)) {
         return user;
       }
